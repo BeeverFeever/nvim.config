e0660770721c1ed1757a58d702eada4efb82ab50
BeeverFeever <scottyadam6@gmail.com>
2024-09-29 20:17:03 +1000

    code execution keybind

--- lua/options/keymaps.lua
+++ lua/options/keymaps.lua
@@ -1,80 +1,73 @@
 local M = {}
 local default_opts = { noremap = true, silent = true }
-local set_keymaps = function(map_table)
-   for _, v in pairs(map_table) do
-      vim.keymap.set(v[1], v[2], v[3], v[4])
-   end
-end
 
 M.general = function()
-   set_keymaps({
-      -- toggle numbers (with relative line numbers)
-      { "n",          "<leader>n",  '<cmd>set rnu! nu!<CR>',         default_opts },
+   -- toggle numbers (with relative line numbers)
+   vim.keymap.set("n", "<leader>n", '<cmd>set rnu! nu!<CR>', default_opts)
 
-      -- use j and k to move through wrapped lines
-      { "n",          "k",          'v:count ? "k" : "gk"',          { silent = true, expr = true } },
-      { "n",          "j",          'v:count ? "j" : "gj"',          { silent = true, expr = true } },
+   -- use j and k to move through wrapped lines
+   vim.keymap.set("n", "k", 'v:count ? "k" : "gk"', { silent = true, expr = true })
+   vim.keymap.set("n", "j", 'v:count ? "j" : "gj"', { silent = true, expr = true })
 
-      -- Yank into system clipboard
-      { { 'n', 'v' }, '<leader>y',  '"+y',                           default_opts },  -- yank motion
-      { { 'n', 'v' }, '<leader>Y',  '"+Y',                           default_opts },  -- yank line
+   -- Yank into system clipboard
+   vim.keymap.set({ 'n', 'v' }, '<leader>y', '"+y', default_opts)
+   vim.keymap.set({ 'n', 'v' }, '<leader>Y', '"+Y', default_opts)
 
-      -- Delete into system clipboard
-      { { 'n', 'v' }, '<leader>d',  '"+d',                           default_opts }, -- delete motion
-      { { 'n', 'v' }, '<leader>D',  '"+D',                           default_opts }, -- delete line
+   -- Delete into system clipboard
+   vim.keymap.set({ 'n', 'v' }, '<leader>d', '"+d', default_opts)
+   vim.keymap.set({ 'n', 'v' }, '<leader>D', '"+D', default_opts)
 
-      -- Paste from system clipboard
-      { { 'n', "v" }, '<leader>p',  '"+p',                           default_opts }, -- paste after cursor
-      { { 'n', "v" }, '<leader>P',  '"+P',                           default_opts }, -- paste before cursor
+   -- Paste from system clipboard
+   vim.keymap.set({ 'n', "v" }, '<leader>p', '"+p', default_opts)
+   vim.keymap.set({ 'n', "v" }, '<leader>P', '"+P', default_opts)
 
-      -- move text around
-      { { "x", "v" }, "<S-j>",      ":m '>+1<CR>gv-gv",              default_opts },
-      { { "x", "v" }, "<S-k>",      ":m '<-2<CR>gv-gv",              default_opts },
+   -- move text around
+   vim.keymap.set({ "x", "v" }, "<S-j>", ":m '>+1<CR>gv-gv", default_opts)
+   vim.keymap.set({ "x", "v" }, "<S-k>", ":m '<-2<CR>gv-gv", default_opts)
 
-      -- stay in visual mode when indenting
-      { "v",          "<",          "<gv",                           default_opts },
-      { "v",          ">",          ">gv",                           default_opts },
+   -- stay in visual mode when indenting
+   vim.keymap.set("v", "<", "<gv", default_opts)
+   vim.keymap.set("v", ">", ">gv", default_opts)
 
-      { "n",          "<leader>tw", "mt<cmd>%s/\\s\\+$//e<CR>`t",    default_opts },
-   })
+   -- remove trailing whitespace
+   vim.keymap.set("n", "<leader>tw", "mt<cmd>%s/\\s\\+$//e<CR>`t", default_opts)
+
+   -- run custom 'build' command
+   vim.keymap.set("n", "<leader>r", "@r")
 end
 
 M.diagnostics = function()
    local diagnostics_opts = { silent = true, noremap = true }
-   set_keymaps({
-      { "n", "<space>e", vim.diagnostic.open_float, diagnostics_opts },
-      { "n", "]d",       vim.diagnostic.goto_next,  diagnostics_opts },
-      { "n", "[d",       vim.diagnostic.goto_prev,  diagnostics_opts },
-      { "n", "<space>q", vim.diagnostic.setloclist, diagnostics_opts },
-   })
+   vim.keymap.set("n", "<space>e", vim.diagnostic.open_float, diagnostics_opts)
+   vim.keymap.set("n", "]d", vim.diagnostic.goto_next, diagnostics_opts)
+   vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, diagnostics_opts)
+   vim.keymap.set("n", "<space>q", vim.diagnostic.setloclist, diagnostics_opts)
 end
 
 M.lsp = function(bufnr)
    local lsp_opts = { silent = true, noremap = true, buffer = bufnr }
-   set_keymaps({
-      { "n",           "<leader>lD", vim.lsp.buf.declaration,    lsp_opts },
-      { "n",           "<leader>ld", vim.lsp.buf.definition,     lsp_opts },
-      { "n",           "<leader>li", vim.lsp.buf.implementation, lsp_opts },
-      { "n",           "<leader>lk", vim.lsp.buf.hover,          lsp_opts },
-      { { "n", "i", }, "<ctrl>h",    vim.lsp.buf.signature_help, lsp_opts },
-      { "n",           "<leader>lr", vim.lsp.buf.rename,         lsp_opts },
-      { "n",           "<leader>la", vim.lsp.buf.code_action,    lsp_opts },
-      { "n",           "<leader>lR", vim.lsp.buf.references,     lsp_opts },
-      {
-         "n",
-         "<leader>f",
-         function()
-            vim.lsp.buf.format({
-               formatting_options = {
-                  tabSize = 3,
-                  insertleaders = true,
-                  trimTrailingWhiteleader = true,
-               },
-            })
-         end,
-         lsp_opts,
-      },
-   })
+   vim.keymap.set({ "n", "i", }, "<ctrl>h", vim.lsp.buf.signature_help, lsp_opts)
+   vim.keymap.set("n", "<leader>lD", vim.lsp.buf.declaration, lsp_opts)
+   vim.keymap.set("n", "<leader>ld", vim.lsp.buf.definition, lsp_opts)
+   vim.keymap.set("n", "<leader>li", vim.lsp.buf.implementation, lsp_opts)
+   vim.keymap.set("n", "<leader>lk", vim.lsp.buf.hover, lsp_opts)
+   vim.keymap.set("n", "<leader>lr", vim.lsp.buf.rename, lsp_opts)
+   vim.keymap.set("n", "<leader>la", vim.lsp.buf.code_action, lsp_opts)
+   vim.keymap.set("n", "<leader>lR", vim.lsp.buf.references, lsp_opts)
+   vim.keymap.set(
+      "n",
+      "<leader>f",
+      function()
+         vim.lsp.buf.format({
+            formatting_options = {
+               tabSize = 3,
+               insertleaders = true,
+               trimTrailingWhiteleader = true,
+            },
+         })
+      end,
+      lsp_opts
+   )
 end
 
 return M
